dd992735fcdd004a6cc32b96ff158c2b056998da
Jeremy Clark <jeremy@jeremybytes.com>
2021-02-07 22:41:06 -0800

    Initial code

---
+++ README.md
@@ -0,0 +1,65 @@
+C# Channels
+=======================
+The aim of this project is to provide an overview of Channel&lt;T&gt; in the C# language. The example presented here mirrors a project in Go (golang). For more information, see [Go for the C# Developer](https://github.com/jeremybytes/go-for-csharp-dev).  
+
+Motivation
+----------
+I first encoutered channels when looking at Go (golang) several years ago. Channels allow for communication between concurrent operations. In .NET Core 3.0, C# got channels as well -- with Channel&lt;T&gt;.  
+
+To compare the usage of channels in C#, this project mirrors a Go (golang) project. This is not an extensive look at channels. It is a brief example of how they can be used.
+
+Project Layout
+--------------
+To build and run the code, you will need to have .NET 5 installed on your machine.
+
+**/async** contains a console application that uses channels program  
+**/net-people-service** contains a service (used by the console application)  
+
+The "async" program is a console application that calls the service and displays the output. In order to show concurrency, the application gets each record individually.
+
+Running the Service
+-------------------  
+The **.NET service** can be started from the command line by navigating to the ".../net-people-service" directory and typing `dotnet run`. This provides endpoints at the following locations:
+
+* http://localhost:9874/people  
+Provides a list of "Person" objects. This service will delay for 3 seconds before responding. Sample result:
+
+```json
+[{"id":1,"givenName":"John","familyName":"Koenig","startDate":"1975-10-17T00:00:00-07:00","rating":6,"formatString":null},  
+{"id":2,"givenName":"Dylan","familyName":"Hunt","startDate":"2000-10-02T00:00:00-07:00","rating":8,"formatString":null}, 
+{...}]
+```
+
+* http://localhost:9874/people/ids  
+Provides a list of "id" values for the collection. Sample:  
+
+```json
+[1,2,3,4,5,6,7,8,9]
+```
+
+* http://localhost:9874/people/1  
+Provides an individual "Person" record based on the "id" value. This service will delay for 1 second before responding. Sample record:
+
+```json
+{"id":1,"givenName":"John","familyName":"Koenig","startDate":"1975-10-17T00:00:00-07:00","rating":6,"formatString":null}
+```
+
+The Console Application
+---------------------
+The **/async** folder contains a console application that uses channels. The relevant code is in the "Program.cs" file.  
+
+Articles
+------------
+A set of articles accompanies this example to walk through how channels are used in conjunction with concurrent (async) operations.  
+
+C# Articles:
+* Using Channels in C# -- *coming soon*  
+
+If you are interested in the Go (golang) sample, there is a set of articles that describe goroutines, channels, WaitGroup, and anonymous functions.
+Go Articles:
+* [Go (golang) Goroutines - Running Functions Asynchronously](https://jeremybytes.blogspot.com/2021/01/go-golang-goroutines-running-functions.html)  
+* [Go (golang) Channels - Moving Data Between Concurrent Processes](https://jeremybytes.blogspot.com/2021/01/go-golang-channels-moving-data-between.html)  
+* [Go (golang) WaitGroup - Signal that a Concurrent Operation is Complete](https://jeremybytes.blogspot.com/2021/02/go-golang-waitgroup-signal-that.html)  
+* [Go (golang) Anonymous Functions - Inlining Code for Goroutines](https://jeremybytes.blogspot.com/2021/02/go-golang-anonymous-functions-inlining.html)  
+
+  
---
+++ async/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace async
+{
+    class Program
+    {
+        public static HttpClient client = new HttpClient() { BaseAddress = new Uri("http://localhost:9874") };
+        public static JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        public static async Task<List<int>> GetIdsAsync()
+        {
+            HttpResponseMessage response =
+                await client.GetAsync("people/ids").ConfigureAwait(false);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new HttpRequestException($"Error fetching URL ({client.BaseAddress}people/ids) return status code {response.StatusCode}");
+            }
+            var stringResult =
+                await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<List<int>>(stringResult);
+        }
+
+        public static async Task<Person> GetPersonAsync(int id)
+        {
+            HttpResponseMessage response =
+                await client.GetAsync($"people/{id}").ConfigureAwait(false);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new HttpRequestException($"Error fetching URL ({client.BaseAddress}people/{id}) return status code {response.StatusCode}");
+            }
+            var stringResult =
+                await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<Person>(stringResult, options);
+        }
+
+        public record Person(int ID, string GivenName, string FamilyName,
+            DateTime StartDate, int Rating, string FormatString)
+        {
+            public override string ToString()
+            {
+                if (string.IsNullOrEmpty(FormatString))
+                    return $"{GivenName} {FamilyName}";
+                return string.Format(FormatString, GivenName, FamilyName);
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var start = DateTime.Now;
+                var ids = await GetIdsAsync();
+                Console.WriteLine(ids.ToDelimitedString(" "));
+
+                var channel = Channel.CreateBounded<Person>(10);
+                var asyncCalls = new List<Task>();
+
+                // Write person objects to channel asynchronously
+                foreach (var id in ids)
+                {
+                    async Task func(int id)
+                    {
+                        try
+                        {
+                            var person = await GetPersonAsync(id);
+                            await channel.Writer.WriteAsync(person);
+                        }
+                        catch (HttpRequestException ex)
+                        {
+                            Console.WriteLine($"ERROR: ID {id}: {ex.Message}");
+                        }
+                    }
+
+                    asyncCalls.Add(func(id));
+                }
+
+                // Wait for the async tasks to complete & close the channel
+                _ = Task.Run(async () =>
+                    {
+                        await Task.WhenAll(asyncCalls);
+                        channel.Writer.Complete();
+                    });
+
+                // Read person objects from the channel until the channel is closed
+                // OPTION 1: Using ReadAllAsync (IAsyncEnumerable)
+                await foreach (Person person in channel.Reader.ReadAllAsync())
+                {
+                    Console.WriteLine($"{person.ID}: {person}");
+                }
+
+                // Read person objects from the channel until the channel is closed
+                // OPTION 2: Using WaitToReadAsync / TryRead
+                // while (await channel.Reader.WaitToReadAsync())
+                // {
+                //    while (channel.Reader.TryRead(out Person person))
+                //    {
+                //        Console.WriteLine($"{person.ID}: {person}");
+                //    }
+                // }
+
+                var elapsed = DateTime.Now - start;
+                Console.WriteLine($"Total Time: {elapsed}");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+        }
+    }
+}
